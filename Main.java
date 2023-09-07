4d408eee26fbb0e7a07b3adf9a9a0da63c1359ba
DIΣGӨ L <di3golabrin@gmail.com>
2023-09-07 15:59:41 -0300

    Código palíndromo versión java editado

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
 public class Main {
 
         public static boolean esPalindromo(String cadena) {
-            StringBuilder resultado = new StringBuilder();
+            StringBuilder palabra_invertida = new StringBuilder();
 
             // Dividir la cadena en un arreglo de caracteres
             char[] caracteres = cadena.toCharArray();
 
             // Invertir el arreglo de caracteres
             for (int i = caracteres.length - 1; i >= 0; i--) {
-                resultado.append(caracteres[i]);
+                palabra_invertida.append(caracteres[i]);
             }
+            String palabra = palabra_invertida.toString();
 
             // Comparar la cadena original con la cadena invertida
-            return cadena.contentEquals(resultado);
+            return cadena.equals(palabra);
         }    public static void main(String[] args) {
         String ejemplo1 = "oso";
         String ejemplo2 = "anilina";
